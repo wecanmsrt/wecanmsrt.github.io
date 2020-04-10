16d71c0b3be2203ea5d8389f1d49bfa2eb549f65
Leo Robinovitch <leorobinovitch@gmail.com>
2020-04-09 23:22:58 -0700

    Updates

---
+++ precache-manifest.c42191469ac2a591f0564d7052a0d368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e658592ea78f1daa6e8f7362a9c4da7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aebf9038fc945f5afaf6",
+    "url": "/static/css/main.6fab1c21.chunk.css"
+  },
+  {
+    "revision": "a21da5909181b7b6c1df",
+    "url": "/static/js/2.5a788b06.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5a788b06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aebf9038fc945f5afaf6",
+    "url": "/static/js/main.44ac54f3.chunk.js"
+  },
+  {
+    "revision": "e3da6409cdf18433ed6d",
+    "url": "/static/js/runtime-main.e95dd661.js"
+  }
+]);
